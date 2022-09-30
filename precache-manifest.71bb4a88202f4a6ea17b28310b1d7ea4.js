1a80bca6ac32f8f48f4692a2c603b34c309ec8dd
kerwindows <kerwindows@hotmail.com>
2022-09-29 21:07:32 -0400

    Updates

---
+++ precache-manifest.71bb4a88202f4a6ea17b28310b1d7ea4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1f33052e9cda16bd1dca11086064c23",
+    "url": "/react-mapping-v1/index.html"
+  },
+  {
+    "revision": "be8c9388e0c2bb835496",
+    "url": "/react-mapping-v1/static/js/2.bc581622.chunk.js"
+  },
+  {
+    "revision": "0343cb6390e379787064",
+    "url": "/react-mapping-v1/static/js/main.ec56b6da.chunk.js"
+  },
+  {
+    "revision": "f0557beb4ad99d07b386",
+    "url": "/react-mapping-v1/static/js/runtime-main.aed1c951.js"
+  }
+]);
